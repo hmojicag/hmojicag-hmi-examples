19143ff01c9c81f1eb60bf39d4a85cda1dcbdca1
Hazael Mojica <hmojica@Hazaels-MacBook-Pro.local>
2017-08-01 15:01:46 -0500

    Adding Arduino code HMI3 Ex.1

---
+++ HMI-3-Arduino-DotNet/HMI-3-Arduino-DotNet.ino
@@ -0,0 +1,91 @@
+/*
+* Hazael Fernando Mojica García
+* 31/July/2017
+* HMI-3-Arduino-.Net-1
+*/
+
+int pinServo = 3;
+int pinLED = 4;
+int pinPush = 5;
+int pinPotA = 0;
+
+int pwmVal = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinServo, OUTPUT);
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinPush, INPUT);
+}
+
+void loop() {
+  if(Serial.available()) {
+    switch(Serial.read()) {
+      case 'a'://Turn On LED
+        digitalWrite(pinLED, HIGH);
+      break;
+      
+      case 'b'://Turn Off LED
+        digitalWrite(pinLED, LOW);
+      break;
+      
+      case 'c'://Increase servo PWM
+        increaseServo();
+      break;
+      
+      case 'd'://Decrease servo PWM
+        decreaseServo();
+      break;
+
+      case 'e'://Read Digital Val
+        readDigitalVal();
+      break;
+
+      case 'f'://Read Analog Value
+        readAnalogVal();
+      break;
+    }
+  }
+}
+
+void increaseServo() {
+  pwmVal += 10;
+  if(pwmVal > 255) {
+    pwmVal = 255;
+  }
+  setPWMVal();
+}
+
+void decreaseServo() {
+  pwmVal -= 10;
+  if(pwmVal < 0) {
+    pwmVal = 0;
+  }
+  setPWMVal();
+}
+
+void setPWMVal() {
+  analogWrite(pinServo, pwmVal);
+  //Write a String of values representing a number
+  //between 0 and 255 and adds \r\n at the end
+  //Example: "0\r\n", "64\r\n", "128\r\n", "255\r\n"
+  Serial.println(pwmVal);
+}
+
+void readDigitalVal() {
+  //Writes "HIGH\r\n" if Push is pressed
+  //writes "LOW\r\n" otherwise
+  if(digitalRead(pinPush)) {
+    Serial.println("HIGH");
+  } else {
+    Serial.println("LOW");
+  }
+}
+
+void readAnalogVal() {
+  //Write a String of values representing a number
+  //between 0 and 1023 and adds \r\n at the end
+  //Example: "0\r\n", "20\r\n", "341\r\n", "1023\r\n",
+  //https://www.arduino.cc/en/Serial/Println
+  Serial.println(analogRead(pinPotA));
+}
