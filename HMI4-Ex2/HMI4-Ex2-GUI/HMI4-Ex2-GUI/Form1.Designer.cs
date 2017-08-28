57a6b7034ee50a77f16006265e7367053a934090
Hazael Mojica <hmojica@Hazaels-MacBook-Pro.local>
2017-08-27 21:14:24 -0500

    HMI4 Ex2 GUI

---
+++ HMI4-Ex2/HMI4-Ex2-GUI/HMI4-Ex2-GUI/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace HMI4_Ex2_GUI {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox_LED = new System.Windows.Forms.CheckBox();
+            this.label_LED = new System.Windows.Forms.Label();
+            this.label_PWM = new System.Windows.Forms.Label();
+            this.label_Digital = new System.Windows.Forms.Label();
+            this.label_Analog = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox_connection = new System.Windows.Forms.GroupBox();
+            this.groupBox_controls = new System.Windows.Forms.GroupBox();
+            this.trackBar_pwm = new System.Windows.Forms.TrackBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox_connection.SuspendLayout();
+            this.groupBox_controls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_pwm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox_LED
+            // 
+            this.checkBox_LED.AutoSize = true;
+            this.checkBox_LED.Location = new System.Drawing.Point(6, 19);
+            this.checkBox_LED.Name = "checkBox_LED";
+            this.checkBox_LED.Size = new System.Drawing.Size(47, 17);
+            this.checkBox_LED.TabIndex = 0;
+            this.checkBox_LED.Text = "LED";
+            this.checkBox_LED.UseVisualStyleBackColor = true;
+            this.checkBox_LED.CheckedChanged += new System.EventHandler(this.checkBox_LED_CheckedChanged);
+            // 
+            // label_LED
+            // 
+            this.label_LED.AutoSize = true;
+            this.label_LED.Location = new System.Drawing.Point(55, 20);
+            this.label_LED.Name = "label_LED";
+            this.label_LED.Size = new System.Drawing.Size(61, 13);
+            this.label_LED.TabIndex = 1;
+            this.label_LED.Text = "LED Status";
+            // 
+            // label_PWM
+            // 
+            this.label_PWM.AutoSize = true;
+            this.label_PWM.Location = new System.Drawing.Point(7, 90);
+            this.label_PWM.Name = "label_PWM";
+            this.label_PWM.Size = new System.Drawing.Size(67, 13);
+            this.label_PWM.TabIndex = 4;
+            this.label_PWM.Text = "PWM Status";
+            // 
+            // label_Digital
+            // 
+            this.label_Digital.AutoSize = true;
+            this.label_Digital.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Digital.Location = new System.Drawing.Point(6, 114);
+            this.label_Digital.Name = "label_Digital";
+            this.label_Digital.Size = new System.Drawing.Size(171, 20);
+            this.label_Digital.TabIndex = 5;
+            this.label_Digital.Text = "Push Button Status:";
+            // 
+            // label_Analog
+            // 
+            this.label_Analog.AutoSize = true;
+            this.label_Analog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Analog.Location = new System.Drawing.Point(6, 143);
+            this.label_Analog.Name = "label_Analog";
+            this.label_Analog.Size = new System.Drawing.Size(81, 20);
+            this.label_Analog.TabIndex = 6;
+            this.label_Analog.Text = "Voltage: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(133, 19);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 8;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox_connection
+            // 
+            this.groupBox_connection.Controls.Add(this.button_connect);
+            this.groupBox_connection.Controls.Add(this.comboBox1);
+            this.groupBox_connection.Location = new System.Drawing.Point(12, 0);
+            this.groupBox_connection.Name = "groupBox_connection";
+            this.groupBox_connection.Size = new System.Drawing.Size(247, 55);
+            this.groupBox_connection.TabIndex = 10;
+            this.groupBox_connection.TabStop = false;
+            this.groupBox_connection.Text = "Serial Connection";
+            // 
+            // groupBox_controls
+            // 
+            this.groupBox_controls.Controls.Add(this.trackBar_pwm);
+            this.groupBox_controls.Controls.Add(this.checkBox_LED);
+            this.groupBox_controls.Controls.Add(this.label_LED);
+            this.groupBox_controls.Controls.Add(this.label_PWM);
+            this.groupBox_controls.Controls.Add(this.label_Analog);
+            this.groupBox_controls.Controls.Add(this.label_Digital);
+            this.groupBox_controls.Location = new System.Drawing.Point(12, 61);
+            this.groupBox_controls.Name = "groupBox_controls";
+            this.groupBox_controls.Size = new System.Drawing.Size(247, 168);
+            this.groupBox_controls.TabIndex = 11;
+            this.groupBox_controls.TabStop = false;
+            this.groupBox_controls.Text = "Controls";
+            // 
+            // trackBar_pwm
+            // 
+            this.trackBar_pwm.Location = new System.Drawing.Point(0, 42);
+            this.trackBar_pwm.Maximum = 200;
+            this.trackBar_pwm.Minimum = 130;
+            this.trackBar_pwm.Name = "trackBar_pwm";
+            this.trackBar_pwm.Size = new System.Drawing.Size(241, 45);
+            this.trackBar_pwm.SmallChange = 5;
+            this.trackBar_pwm.TabIndex = 7;
+            this.trackBar_pwm.TickFrequency = 5;
+            this.trackBar_pwm.Value = 130;
+            this.trackBar_pwm.ValueChanged += new System.EventHandler(this.trackBar_pwm_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 240);
+            this.Controls.Add(this.groupBox_controls);
+            this.Controls.Add(this.groupBox_connection);
+            this.Name = "Form1";
+            this.Text = "Human Machine Interface";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox_connection.ResumeLayout(false);
+            this.groupBox_controls.ResumeLayout(false);
+            this.groupBox_controls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_pwm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox_LED;
+        private System.Windows.Forms.Label label_LED;
+        private System.Windows.Forms.Label label_PWM;
+        private System.Windows.Forms.Label label_Digital;
+        private System.Windows.Forms.Label label_Analog;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox_connection;
+        private System.Windows.Forms.GroupBox groupBox_controls;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TrackBar trackBar_pwm;
+    }
+}
+
---
+++ HMI4-Ex2/HMI4-Ex2-GUI/HMI4-Ex2-GUI/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Collections.Generic;
+
+namespace HMI4_Ex2_GUI {
+    public partial class Form1 : Form {
+        private Queue<CVPair> dataQueue;
+        private string incomeBuffer;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox1.Items.Clear();
+            //Add a default Message to the combo box
+            comboBox1.Items.Add("Select a Serial Port");
+            //Fill the ComboBox with the names of the Serial Ports
+            string[] availableSerialPorts = SerialPort.GetPortNames();
+            foreach(string portName in availableSerialPorts) {
+                comboBox1.Items.Add(portName);
+            }
+            //Choose the "Select a Serial Port" msg for display
+            comboBox1.SelectedIndex = 0;
+            //Disable Controls Group Box
+            groupBox_controls.Enabled = false;
+            //Initialize Income data Queue
+            dataQueue = new Queue<CVPair>();
+            incomeBuffer = string.Empty;
+        }
+
+        private void trackBar_pwm_ValueChanged(object sender, EventArgs e) {
+            //Send command to Set PWM to trackbar value
+            SendData(2, trackBar_pwm.Value);
+        }
+
+        private void button_connect_Click(object sender, EventArgs e) {
+            if(comboBox1.SelectedIndex > 0) {
+                //A valid Serial Port has been selected
+                serialPort1.BaudRate = 115200;
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                try {
+                    serialPort1.Open();
+                } catch (Exception ex) {
+                    timer1.Stop();
+                    Console.WriteLine(ex);
+                    MessageBox.Show("Error connection to port: "
+                        + serialPort1.PortName);
+                    this.Close();
+                }
+                //enable the timer
+                timer1.Enabled = true;
+                timer1.Start();
+                //Swap Group Box
+                groupBox_connection.Enabled = false;
+                groupBox_controls.Enabled = true;
+            }
+        }
+
+        private void checkBox_LED_CheckedChanged(object sender, EventArgs e) {
+            if(checkBox_LED.Checked) {
+                SendData(1, 1);//Send LED command ON
+            } else {
+                SendData(1, 0);//Send LED command OFF
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            bool dataSent;
+            dataSent = SendData(3);//Send Read Digital command
+            dataSent = dataSent | SendData(4);//Send Read Analog command
+
+            if(!dataSent) {
+                //Error sending data, stop Tick
+                timer1.Stop();
+                timer1.Enabled = false;
+                MessageBox.Show("Error sending data to the Arduino");
+                this.Close();
+            }
+            ParseBuffer();
+            RefreshGUI();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            timer1.Stop();
+            timer1.Enabled = false;
+            serialPort1.Close();
+        }
+
+        private void ParseBuffer() {
+            //This is the tricky code that stores all data received
+            //parses and stores it in a List of command value pairs
+            try {
+                if (serialPort1.IsOpen) {
+                    //Read everything the Arduino has sent so far
+                    incomeBuffer += serialPort1.ReadExisting();
+                }
+                //Split string into c-v pairs
+                //For example: "digital-HIGH&analog-512&led-HIGH&"
+                //will be split in { "digital-HIGH", "analog-512", "led-HIGH", ""}
+                string[] cvPairs = incomeBuffer.Split('&');
+                incomeBuffer = cvPairs[cvPairs.Length - 1];
+                for (int i = 0; i < (cvPairs.Length-1); i++) {
+                    string[] cvPair = cvPairs[i].Split('-');
+                    if(cvPair.Length == 2) {
+                        //If the pair has not exactly 2 elements
+                        //a command and a value, discard it
+                        CVPair pair = new CVPair();
+                        pair.Command = cvPair[0];
+                        pair.Value = cvPair[1];
+                        dataQueue.Enqueue(pair);
+                        Console.WriteLine("Command Enqued: " + cvPairs[i]);
+                    } else {
+                        Console.WriteLine("Command Discarded: " + cvPairs[i]);
+                    }
+                }
+            } catch(Exception ex) {
+                timer1.Stop();
+                timer1.Enabled = false;
+                Console.WriteLine(ex);
+                MessageBox.Show("Error reading data from the Arduino");
+                this.Close();
+            }
+        }
+
+        private void RefreshGUI() {
+            foreach(CVPair pair in dataQueue) {
+                switch(pair.Command) {
+                    case "led":
+                        label_LED.Text = "LED Status: " + pair.Value;
+                        break;
+                    case "pwm":
+                        label_PWM.Text = "PWM Val: " + pair.Value;
+                        break;
+                    case "digital":
+                        label_Digital.Text = "Push Button Status: " + pair.Value;
+                        break;
+                    case "analog":
+                        //Convert to Double the ADC value
+                        double adcVal = Convert.ToDouble(pair.Value);
+                        //The ADC maps the 0 to 5v in 10bits (0 to 1023)
+                        double volts = adcVal * 5.0 / 1023.0;
+                        label_Analog.Text = "Voltage: " + volts.ToString("##.##") + "V";
+                        break;
+                    default:
+                        Console.WriteLine("Not a recognized response command: " + pair.Command);
+                        break;
+                }
+            }
+        }
+
+        private bool SendData(int command) {
+            return SendData(command, 0);
+        }
+
+        private bool SendData(int command, int value) {
+            bool allOk = false;
+            if(serialPort1.IsOpen) {
+                try {
+                    string request = "&" + command + "-" + value + "&";
+                    serialPort1.Write(request);
+                    Console.WriteLine("Request sent: " + request);
+                    allOk = true;
+                } catch(Exception ex) {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            return allOk;
+        }
+
+        private class CVPair {
+            public string Command { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
